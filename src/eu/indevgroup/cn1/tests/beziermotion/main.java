21f0a1647f93e5fe0d62a2ab71e1363a0f4244cf
A. Cherepanov <ftp27host@gmail.com>
2015-05-25 18:37:15 +0300

    Add spinners

--- src/eu/indevgroup/cn1/tests/beziermotion/main.java
+++ src/eu/indevgroup/cn1/tests/beziermotion/main.java
@@ -7,12 +7,14 @@ import com.codename1.ui.Container;
 import com.codename1.ui.Display;
 import com.codename1.ui.Form;
 import com.codename1.ui.Label;
+import com.codename1.ui.Slider;
 import com.codename1.ui.events.ActionEvent;
 import com.codename1.ui.events.ActionListener;
 import com.codename1.ui.layouts.BorderLayout;
 import com.codename1.ui.layouts.GridLayout;
 import com.codename1.ui.plaf.UIManager;
 import com.codename1.ui.spinner.NumericSpinner;
+import com.codename1.ui.table.TableLayout;
 import com.codename1.ui.util.Resources;
 import java.io.IOException;
 
@@ -36,46 +38,61 @@ public class main {
         }
         Form hi = new Form("Custom Motion Test");
         Container controlContainer = new Container(new BorderLayout());
-        Container spinnersContainer = new Container (new GridLayout(1, 4));
+        Container spinnersContainer = new Container (new TableLayout(4, 2));
         
         final BezierVisualisator visualisator = new BezierVisualisator();
         visualisator.setBezier(0.0, 0.0, 0.0, 0.0);
         controlContainer.addComponent(BorderLayout.CENTER, visualisator);
         
-        final NumericSpinner beginX = new NumericSpinner();
-        final NumericSpinner beginY = new NumericSpinner();
-        final NumericSpinner endX = new NumericSpinner();
-        final NumericSpinner endY = new NumericSpinner();
-        beginX.setMax(2.0);   beginY.setMax(2.0);   endX.setMax(2.0);   endY.setMax(2.0);
-        beginX.setMin(-2.0);  beginY.setMin(-2.0);  endX.setMin(-2.0);  endY.setMin(-2.0);
-        beginX.setValue(0.0); beginY.setValue(0.0); endX.setValue(0.0); endY.setValue(0.0);
-        beginX.setStep(0.1); beginY.setStep(0.1); endX.setStep(0.1); endY.setStep(0.1); 
-        spinnersContainer.addComponent(beginX); spinnersContainer.addComponent(beginY);
-        spinnersContainer.addComponent(endX);   spinnersContainer.addComponent(endY);
+        final double minValue = -2.0;
+        final double maxValue = 2.0;
+        final double div = (maxValue-minValue)/100;
         
-        ActionListener onChange = new ActionListener() {
+        final Label beginXl = new Label("0.0");
+        final Label beginYl = new Label("0.0");
+        final Label endXl = new Label("0.0");
+        final Label endYl = new Label("0.0");
+        
+        final Slider beginX = new Slider();
+        final Slider beginY = new Slider();
+        final Slider endX = new Slider();
+        final Slider endY = new Slider();
+        beginX.setProgress(50); beginY.setProgress(50); endX.setProgress(50); endY.setProgress(50);
+        beginX.setEditable(true); beginY.setEditable(true); endX.setEditable(true); endY.setEditable(true);
+        spinnersContainer.addComponent(beginX); spinnersContainer.addComponent(beginXl);
+        spinnersContainer.addComponent(beginY); spinnersContainer.addComponent(beginYl);
+        spinnersContainer.addComponent(endX);   spinnersContainer.addComponent(endXl);
+        spinnersContainer.addComponent(endY);   spinnersContainer.addComponent(endYl);
+        
+        ActionListener onChange;
+        onChange = new ActionListener() {
             public void actionPerformed(ActionEvent evt) {
                 visualisator.setBezier(
-                    beginX.getValue(), 
-                    beginY.getValue(), 
-                    endX.getValue(), 
-                    endY.getValue()
+                        beginX.getProgress()*div+minValue,
+                        beginY.getProgress()*div+minValue,
+                        endX.getProgress()*div+minValue,
+                        endY.getProgress()*div+minValue
                 );
+                beginXl.setText(new Double(beginX.getProgress()*div+minValue).toString());
+                beginYl.setText(new Double(beginY.getProgress()*div+minValue).toString());
+                endXl.setText(new Double(endX.getProgress()*div+minValue).toString());
+                endYl.setText(new Double(endY.getProgress()*div+minValue).toString());
             }
         };
-       // beginX.listener(onChange);
-        beginY.addPointerPressedListener(onChange);
-        endX.addPointerPressedListener(onChange);
-        endY.addPointerPressedListener(onChange);
+        beginX.addActionListener(onChange);
+        beginY.addActionListener(onChange);
+        endX.addActionListener(onChange);
+        endY.addActionListener(onChange);
         
-        Button bt = new Button("change");
+        Button bt = new Button("animate");
         bt.addActionListener(onChange);
-        
         
         controlContainer.addComponent(BorderLayout.SOUTH, spinnersContainer);
         hi.setLayout(new BorderLayout());
+        
         hi.addComponent(BorderLayout.CENTER, controlContainer);
         hi.addComponent(BorderLayout.SOUTH, bt);
+
         hi.show();
     }
 
